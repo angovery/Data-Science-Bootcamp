d850b48066e94b5f9ed4976af0b2f07d057061e3
angovery <109955309+angovery@users.noreply.github.com>
2025-03-01 20:46:37 +0100

    Add files via upload

---
+++ Ejercicios_Evaluados/Modulo_7/Inicio.py
@@ -0,0 +1,12 @@
+# Home.py
+import streamlit as st
+
+st.set_page_config(page_title="Diamonds Analysis", layout="wide")
+
+st.title("Análisis del Dataset 'Diamonds'")
+st.markdown("""
+Esta aplicación realiza un análisis exploratorio del famoso dataset **diamonds** y permite:
+- Visualizar gráficos univariantes, bivariantes y multivariantes.
+- Realizar predicciones de precio mediante un modelo de regresión.
+- Predecir la categoría del corte de los diamantes mediante un modelo de clasificación.
+""")
