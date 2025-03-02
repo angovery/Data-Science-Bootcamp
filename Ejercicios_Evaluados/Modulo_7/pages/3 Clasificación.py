a9adf3b622c469aaa289ea290b48086e9056abf2
angovery <azkenmuga@hotmail.com>
2025-03-02 09:42:26 +0100

    Actualización.

--- Ejercicios_Evaluados/Modulo_7/pages/3 Clasificación.py
+++ Ejercicios_Evaluados/Modulo_7/pages/3 Clasificación.py
@@ -48,4 +48,4 @@ if st.button("Predecir corte"):
     # Se decodifica el valor numérico de la prediccióm para mostrar el valor asociado de los posibles valores de la variable.
     etiqueta_predicha = label_encoder.inverse_transform(prediccion)
     # Mostrar el resultado.
-    st.write("El corte predicho es:", etiqueta_predicha[0])
+    st.metric("El corte predicho es:", etiqueta_predicha[0])
